007c67f884e8fa36e6e63acfc450122c9483ca14
hemasaviant <hemlata@saviantconsuting.com>
2014-03-05 18:07:19 +0530

    committ changes - updated SDK 2.2

--- twitterbatchprocess/WorkerRole.cs
+++ twitterbatchprocess/WorkerRole.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Threading;
 using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Diagnostics;
+//using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
